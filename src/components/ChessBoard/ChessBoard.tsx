348232d799e97b52a85f9a42caa379742facc1ec
TraMyNguyen39 <tram8319@gmail.com>
2024-04-02 00:11:50 +0700

    insert pieces into chessboard

---
+++ src/components/ChessBoard/ChessBoard.tsx
@@ -0,0 +1,53 @@
+import Tile from '../Tile/Tile'
+import './ChessBoard.css'
+
+const verticalAxis = ['1', '2', '3', '4', '5', '6', '7', '8']
+const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+interface Piece {
+  image: string
+  x: number
+  y: number
+}
+
+const pieces: Piece[] = []
+
+for (let p = 0; p < 2; p++) {
+  const type = p === 0 ? 'b' : 'w'
+  const y = p === 0 ? 7 : 0
+
+  pieces.push({ image: `assets/images/rook_${type}.png`, x: 0, y: y })
+  pieces.push({ image: `assets/images/rook_${type}.png`, x: 7, y: y })
+  pieces.push({ image: `assets/images/knight_${type}.png`, x: 1, y: y })
+  pieces.push({ image: `assets/images/knight_${type}.png`, x: 6, y: y })
+  pieces.push({ image: `assets/images/bishop_${type}.png`, x: 2, y: y })
+  pieces.push({ image: `assets/images/bishop_${type}.png`, x: 5, y: y })
+  pieces.push({ image: `assets/images/queen_${type}.png`, x: 3, y: y })
+  pieces.push({ image: `assets/images/king_${type}.png`, x: 4, y: y })
+}
+
+for (let i = 0; i < 8; i++) {
+  pieces.push({ image: 'assets/images/pawn_b.png', x: i, y: 6 })
+}
+
+for (let i = 0; i < 8; i++) {
+  pieces.push({ image: 'assets/images/pawn_w.png', x: i, y: 1 })
+}
+
+export default function ChessBoard() {
+  const board = []
+  for (let j = verticalAxis.length - 1; j >= 0; j--) {
+    for (let i = 0; i < horizontalAxis.length; i++) {
+      let image = undefined
+
+      pieces.forEach((piece) => {
+        if (piece.x === i && piece.y === j) {
+          image = piece.image
+        }
+      })
+      board.push(<Tile number_row={i} number_column={j} image={image} />)
+    }
+  }
+
+  return <div id='checkboard'>{board}</div>
+}
---
+++ src/components/Tile/Tile.tsx
@@ -0,0 +1,23 @@
+import './Tile.css'
+
+interface Props {
+  image?: string
+  number_row: number
+  number_column: number
+}
+
+export default function Tile({ image, number_row, number_column }: Props) {
+  if (number_row % 2 === number_column % 2) {
+    return (
+      <div className='tile black-tile'>
+        <img src={image} alt='' />
+      </div>
+    )
+  } else {
+    return (
+      <div className='tile white-tile'>
+        <img src={image} alt='' />
+      </div>
+    )
+  }
+}

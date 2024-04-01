9293f476e6d042ce7c8e6439f643cd8fdc128b16
TonNQ <nguyenquoctoan12122003@gmail.com>
2024-04-01 19:07:31 +0700

    init & config project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E90ff',
+        secondary: '#5c9fe0'
+      }
+    }
+  }
+}

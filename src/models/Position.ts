7954271fee73a574e5e0bd4c8703c8a3edb2413f
TonNQ <nguyenquoctoan12122003@gmail.com>
2024-04-04 10:26:11 +0700

    refactor code by Position model and code movement of the knight piece

---
+++ src/models/Position.ts
@@ -0,0 +1,16 @@
+export class Position {
+  x: number
+  y: number
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+
+  samePosition(other: Position): boolean {
+    return this.x === other.x && this.y === other.y
+  }
+
+  copy(): Position {
+    return new Position(this.x, this.y)
+  }
+}

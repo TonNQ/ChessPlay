16c477b20ff4204886eda9887e736858c1015579
TonNQ <nguyenquoctoan12122003@gmail.com>
2024-04-09 21:32:35 +0700

    protect king by moving pieces to prevent attack or moving king piece

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,26 @@
 /* eslint-disable import/no-unresolved */
+import boardApi from 'src/apis/board.api'
 import ChessStock from '/assets/images/chess-stock.jpg'
+import { setGameIdToLocalStorage } from 'src/utils/storage'
+import { toast } from 'react-toastify'
 
 interface Props {
   setIsGameExist: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export default function Home({ setIsGameExist }: Props) {
+  const handleClick = () => {
+    boardApi
+      .createGame()
+      .then((response) => {
+        setGameIdToLocalStorage(response.data.id)
+        setIsGameExist(true)
+      })
+      .catch((error) => {
+        toast.error(error.message)
+      })
+  }
+
   return (
     <div className='relative h-[100vh] w-[100vw]'>
       <div className='pixel-font absolute left-1/2 top-1/4 z-50 -translate-x-1/2 -translate-y-1/2 transform text-9xl uppercase text-white/85'>
@@ -14,7 +29,7 @@ export default function Home({ setIsGameExist }: Props) {
       <img alt='ảnh' src={ChessStock} className='absolute h-full w-full object-cover opacity-90' />
       <button
         className='pixel-font absolute bottom-[120px] left-1/2 z-50 -translate-x-1/2 transform border-none bg-lightBrown text-2xl uppercase hover:bg-darkLayout'
-        onClick={() => setIsGameExist(true)}
+        onClick={handleClick}
       >
         Play new game
       </button>
